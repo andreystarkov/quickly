3fb6258acab9a811367d13126601e2cb243377c2
Andrey Starkov <im@andreystarkov.ru>
2016-02-26 13:23:08 +0500

    :C

--- app/scripts/engine.js
+++ app/scripts/engine.js
@@ -2,4 +2,24 @@
 
 $(function() {
 
+        $.ajax( {
+            type: 'GET',
+            datatype: 'jsonp',
+            data: {},
+            crossDomain: 'true',
+            url: "http://176.112.201.81/api/v2/restaurants/get?restaurantType=3",
+            error: function(textStatus, errorThrown) {
+                console.log(textStatus+' :(( '+errorThrown);
+            },
+            success: function(msg) {
+                console.log(msg);
+            }
+        });
+/*    $.getJSON('http://176.112.201.81/api/v2/restaurants/get?restaurantType=3', { get_param: 'result' }, function(data) {
+        console.log(data);
+        $.each(data, function(index, element) {
+            $('#section-list').append(element);
+            console.log(index+' - '+element);
+        });
+    });*/
 });
